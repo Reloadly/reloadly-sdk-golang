f14aefc230991f4b3b7c52843b0f8522b025b4dc
Ghvstcode <impoissants@gmail.com>
2021-05-19 11:23:49 +0100

    Fix broken Tests

--- airtime/operators_test.go
+++ airtime/operators_test.go
@@ -63,27 +63,43 @@ func TestClient_GetOperatorsByISO(t *testing.T) {
 	defer teardown()
 
 	mux.HandleFunc("/", func(rw http.ResponseWriter, req *http.Request) {
+		rw.WriteHeader(http.StatusInternalServerError)
+	})
+
+	body, err := client.GetOperatorsByISO("")
+
+	if err == nil {
+		t.Errorf("Expected error but got nil")
+	}
+
+	if body != nil {
+		t.Errorf("Expected body to be nil but got %v",  body)
+	}
+}
+
+
+func TestClient_GetOperatorsById(t *testing.T) {
+	teardown := setup()
+
+	defer teardown()
+
+	mux.HandleFunc("/", func(rw http.ResponseWriter, req *http.Request) {
 		rw.WriteHeader(http.StatusOK)
 		data := reloadly.Operators{
-			TotalPages:    5,
-			Size:             20,
+			Name: "testOperator",
 		}
-		//data := reloadly.Operators{
-		//	TotalPages:    5,
-		//	Size: 20,
-		//}
 
 		json.NewEncoder(rw).Encode(data)
 
 	})
 
-	body, err := client.GetOperatorsByISO("011")
+	body, err := client.GetOperatorsById(7)
 	if err != nil {
 		t.Errorf("Expected error to be nil but got %q",  err)
 	}
 
-	if body.Size != 20 {
-		t.Errorf("Expected Size to be 20 but got %v",  body.Size)
+	if body.Name != "testOperator" {
+		t.Errorf("Expected Size to be testOperator but got %v",  body.Name)
 	}
 }
 
@@ -93,38 +109,115 @@ func TestClient_GetOperators(t *testing.T) {
 	defer teardown()
 
 	mux.HandleFunc("/operators", func(rw http.ResponseWriter, req *http.Request) {
-		rw.WriteHeader(http.StatusOK)
-		data := reloadly.Operators{
-			TotalPages:    5,
-			Size: 20,
-		}
-
-		json.NewEncoder(rw).Encode(data)
-
+		rw.WriteHeader(http.StatusInternalServerError)
 	})
 
 	body, err := client.GetOperators()
-	if err != nil {
-		t.Errorf("Expected error to be nil but got %q",  err)
+
+	if err == nil {
+		t.Errorf("Expected error but got nil")
 	}
 
-	if body.Size != 20 {
-		t.Errorf("Expected Size to be 20 but got %v",  body.Size)
+	if body != nil {
+		t.Errorf("Expected body to be nil but got %v",  body)
 	}
 }
 
-func TestClient_GetOperatorsById(t *testing.T) {
-	teardown := setup()
+func TestAddSuggestedAmounts(t *testing.T) {
+	cases := [] struct{
+		SuggestedAmounts bool
+		ExpectedSuggestedAmounts bool
+	}{
+		{
+			SuggestedAmounts: false,
+			ExpectedSuggestedAmounts: false,
+		},
+		{
+			SuggestedAmounts: true,
+			ExpectedSuggestedAmounts: true,
+		},
+	}
 
-	defer teardown()
 
-	mux.HandleFunc("/", func(rw http.ResponseWriter, req *http.Request) {
-		rw.WriteHeader(http.StatusInternalServerError)
 
-	})
+	for _, c := range cases {
+		res := reloadly.AddSuggestedAmounts(c.SuggestedAmounts)
+		o := &reloadly.OperatorOpts{}
+		res(o)
 
-	_, err := client.GetOperators()
-	if err == nil {
-		t.Errorf("Expected error but got nil")
+		if res != nil{
+			if c.ExpectedSuggestedAmounts != o.SuggestedAmounts {
+				t.Fatalf("Expected SuggestedAmounts to be %t but got %t", c.SuggestedAmounts, o.SuggestedAmounts)
+			}
+
+		}
+
+
+	}
+}
+
+func TestAddBundles(t *testing.T) {
+	cases := [] struct{
+		IncludeBundles bool
+		ExpectedIncludeBundles bool
+	}{
+		{
+			IncludeBundles: false,
+			ExpectedIncludeBundles: false,
+		},
+		{
+			IncludeBundles: true,
+			ExpectedIncludeBundles: true,
+		},
+	}
+
+
+
+	for _, c := range cases {
+		res := reloadly.AddBundles(c.IncludeBundles)
+		o := &reloadly.OperatorOpts{}
+		res(o)
+
+		if res != nil{
+			if c.ExpectedIncludeBundles != o.IncludeBundles {
+				t.Fatalf("Expected SuggestedAmounts to be %t but got %t", c.IncludeBundles, o.IncludeBundles)
+			}
+
+		}
+
+
+	}
+}
+
+func TestAddData(t *testing.T) {
+	cases := [] struct{
+		IncludeData bool
+		ExpectedIncludeData bool
+	}{
+		{
+			IncludeData: false,
+			ExpectedIncludeData: false,
+		},
+		{
+			IncludeData: true,
+			ExpectedIncludeData: true,
+		},
+	}
+
+
+
+	for _, c := range cases {
+		res := reloadly.AddData(c.IncludeData)
+		o := &reloadly.OperatorOpts{}
+		res(o)
+
+		if res != nil{
+			if c.IncludeData != o.IncludeData {
+				t.Fatalf("Expected SuggestedAmounts to be %t but got %t", c.IncludeData, o.IncludeData)
+			}
+
+		}
+
+
 	}
 }
